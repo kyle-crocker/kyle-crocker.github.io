8cafb779d08d7e06024826851285ffbae2cf334d
karnagowda <karna.gowda@gmail.com>
2022-01-23 09:20:02 -0600

    Build website

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 hugo                               
 cd public
+git pull https://github.com/karnagowda/karnagowda.github.io master
 git add --all
 git commit -m "Build website"
 git pull https://github.com/karnagowda/karnagowda.github.io master
