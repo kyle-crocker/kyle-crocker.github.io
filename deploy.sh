c6c7ab862a86e31e5ad4ff2c34b94d736959d96a
karnagowda <karna.gowda@gmail.com>
2022-01-23 09:47:16 -0600

    Build website

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 hugo                               
-# cd public
 git add .
 git commit -m "Build website"
 git push origin master
 cd public
-git add .
 git commit -m "Build website"
 git push https://github.com/karnagowda/karnagowda.github.io master
 cd ..
